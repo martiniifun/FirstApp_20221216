2f0aedfbb74711c02bd547718173cd6f985dde1d
Martin Shin <martinii.fun@gmail.com>
2022-12-16 22:22:08 +0900

    불필요한 using 제거

--- conditional_20221216/Form1.cs
+++ conditional_20221216/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace conditional_20221216
@@ -20,31 +13,21 @@ namespace conditional_20221216
 
         private void btnResult_Click(object sender, EventArgs e)
         {
-            int kuk = int.Parse(tbKuk.Text);
-            int eng = int.Parse(tbEng.Text);
-            int math = int.Parse(tbMath.Text);
-            int total = kuk + eng + math;
+            var kuk = int.Parse(tbKuk.Text);
+            var eng = int.Parse(tbEng.Text);
+            var math = int.Parse(tbMath.Text);
+            var total = kuk + eng + math;
             lblAvg.Text = (total / 3).ToString();
-            if (total/3 >= 90)
-            {
+            if (total / 3 >= 90)
                 lblResult.Text = "A학점";
-            }
-            else if (total/3 >= 80)
-            {
+            else if (total / 3 >= 80)
                 lblResult.Text = "B학점";
-            }
-            else if (total/3 >= 70)
-            {
+            else if (total / 3 >= 70)
                 lblResult.Text = "C학점";
-            }
-            else if (total/3 >= 60)
-            {
+            else if (total / 3 >= 60)
                 lblResult.Text = "D학점";
-            }
             else
-            {
                 lblResult.Text = "F학점";
-            }
         }
 
         private void tbKuk_Enter(object sender, EventArgs e)
